f3153fffd6f7b9e09ca8b6ee63d32ebdd8562faf
ajust <ajust@DESKTOP-0TCMI8I.mshome.net>
2025-02-21 18:27:43 +0800

    System project collab

---
+++ src/java/MyServlet.java
@@ -0,0 +1,27 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/MyServlet"})
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html");
+        
+        PrintWriter out = response.getWriter();
+        
+        int result = Integer.parseInt(request.getParameter("numberWan")) + Integer.parseInt(request.getParameter("numberTo"));
+        
+        out.println("Output: " + result);
+       
+    }
+
+}
